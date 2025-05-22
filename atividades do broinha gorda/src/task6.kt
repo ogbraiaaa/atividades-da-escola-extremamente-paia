73543e216402d3eabe76d5fb1c18a2c4ce338e4d
braiaaa <brayangui171@gmail.com>
2025-05-21 22:52:43 -0300

    task6.kt

--- atividades do broinha gorda/src/task6.kt
+++ atividades do broinha gorda/src/task6.kt
@@ -1,15 +1,15 @@
 fun main() {
-    val array = IntArray(10)
+    var array = IntArray(10)
     var contadorPares = 0
 
     println("Digite 10 números inteiros:")
     for (i in 0 until 10) {
-        val entrada = readlnOrNull()?.toIntOrNull()
+        var entrada = readlnOrNull()?.toIntOrNull()
         if (entrada != null) {
             array[i] = entrada
         } else {
             println("Entrada inválida usando 0 para a posição $i.")
-            array[i] = 0 // Valor padrão em caso de entrada inválida
+            array[i] = 0
         }
     }
 
