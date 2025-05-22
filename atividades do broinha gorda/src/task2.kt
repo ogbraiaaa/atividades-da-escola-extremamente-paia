32f2ec050aa1d30c6810f4ef91868ebc25bd72c6
braiaaa <brayangui171@gmail.com>
2025-05-21 22:47:49 -0300

    task2.kt

--- atividades do broinha gorda/src/task2.kt
+++ atividades do broinha gorda/src/task2.kt
@@ -1,18 +1,18 @@
 fun fibonacci(n: Int): List<Int> {
-    val lista = mutableListOf<Int>()
+    var lista = mutableListOf<Int>()
     if (n >= 1) lista.add(1)
     if (n >= 2) lista.add(1)
     for (i in 2 until n) {
-        val proximo = lista[i - 1] + lista[i - 2]
+        var proximo = lista[i - 1] + lista[i - 2]
         lista.add(proximo)
     }
     return lista
 }
 
 fun main() {
-    val n = readlnOrNull()?.toIntOrNull()
+    var n = readlnOrNull()?.toIntOrNull()
     if (n != null && n > 0) {
-        val resultado = fibonacci(n)
+        var resultado = fibonacci(n)
         println(resultado.joinToString(", "))
     }
 }
