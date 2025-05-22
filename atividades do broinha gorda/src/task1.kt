b5d6ca85ef309bc12614ed3a8765d51b28d5dbe8
braiaaa <brayangui171@gmail.com>
2025-05-21 22:46:36 -0300

    task1.kt

--- atividades do broinha gorda/src/task1.kt
+++ atividades do broinha gorda/src/task1.kt
@@ -1,22 +1,26 @@
 import kotlin.math.sqrt
 
-fun calcularTempoAberturaSemaforo(distancia: Double, velocidade: Double, aceleracao: Double): Double {
-    val tempoParaAtingirVelocidade = velocidade / aceleracao
-    val distanciaAteVelocidade = 0.5 * aceleracao * tempoParaAtingirVelocidade * tempoParaAtingirVelocidade
-    val tempoParaPercorrerDistancia = if (distancia > distanciaAteVelocidade) {
-        val distanciaRestante = distancia - distanciaAteVelocidade
-        tempoParaAtingirVelocidade + (distanciaRestante / velocidade)
+fun calcularTempoAberturaSemaforo(distancia: Int, velocidade: Int, aceleracao: Int): Int {
+    var tempoParaAtingirVelocidade = velocidade / aceleracao
+    var distanciaAteVelocidade = (0.5 * aceleracao * tempoParaAtingirVelocidade * tempoParaAtingirVelocidade).toInt()
+
+    var tempoParaPercorrerDistancia: Int
+
+    if (distancia > distanciaAteVelocidade) {
+        var distanciaRestante = distancia - distanciaAteVelocidade
+        tempoParaPercorrerDistancia = tempoParaAtingirVelocidade + (distanciaRestante / velocidade)
     } else {
-        val tempoParaParar = sqrt((2 * distancia) / aceleracao)
-        tempoParaParar
+        var tempCalculo = (2.0 * distancia) / aceleracao
+        tempoParaPercorrerDistancia = sqrt(tempCalculo).toInt()
     }
+
     return tempoParaPercorrerDistancia + 3
 }
 
 fun main() {
-    val distancia = 100.0
-    val velocidade = 20.0
-    val aceleracao = 2.0
-    val tempoAbertura = calcularTempoAberturaSemaforo(distancia, velocidade, aceleracao)
+    var distancia = 100
+    var velocidade = 20
+    var aceleracao = 2
+    var tempoAbertura = calcularTempoAberturaSemaforo(distancia, velocidade, aceleracao)
     println("o semaforo deve abrir $tempoAbertura segundos antes")
 }
